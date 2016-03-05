77d62b1aa5ad271fa9ea81a48e41285edaadd182
Phuoc <phuocpeter19@gmail.com>
2016-03-05 17:50:15 -0500

    Initial Commit

---
+++ Phuoc Web Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Phuoc Web Client
+//
+//  Created by Tran Thai Phuoc on 2016-03-05.
+//  Copyright © 2016 Tran Thai Phuoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
