a5a31c51c43cb8757d3b7205f057ea7454f95de6
Phuoc <phuocpeter19@gmail.com>
2016-03-05 19:44:39 -0500

    Established connection

--- Phuoc Web Client/LoginViewController.swift
+++ Phuoc Web Client/LoginViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class LoginViewController: UITableViewController {
+    
+    @IBOutlet weak var serverTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,42 @@ class LoginViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Table View Delegate Methods
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 
+    // MARK: - IBAction Methods
+    
+    @IBAction func login() {
+        postToServer()
+
+    }
+    
+    @IBAction func serverDone(sender: AnyObject) {
+        usernameTextField.becomeFirstResponder()
+    }
+    @IBAction func usernameDone(sender: AnyObject) {
+        passwordTextField.becomeFirstResponder()
+    }
+    @IBAction func passwordDone(sender: AnyObject) {
+        passwordTextField.endEditing(false)
+    }
+    // MARK: - Server connection Methods
+    
+    func postToServer() {
+        let url = NSURL(string: "http://192.168.1.2")
+        let request = NSMutableURLRequest(URL: url!)
+        let username = "name=\(usernameTextField.text)"
+        let password = "key=\(passwordTextField.text)"
+        request.HTTPMethod = "POST"
+        request.HTTPBody = username.dataUsingEncoding(NSUTF8StringEncoding)
+        request.HTTPBody = password.dataUsingEncoding(NSUTF8StringEncoding)
+        print("Posted")
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response, data, error) in print(response, error)})
+    }
 
 }
 
